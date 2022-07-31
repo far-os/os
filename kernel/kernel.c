85d91047f0d70b093d817bf121c604063a794dc2
Fire Fish <mailsodd@gmail.com>
2022-07-31 16:05:07 +0100

    haha readme repairs go brrrrr

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,19 @@
 #include "text.h"
 
+void cp437() {
+  // prints cp437
+
+  write_str("  Codepage 437  ", POS((VGA_WIDTH - 16), (VGA_HEIGHT - 16 - 2)), COLOUR(MAGENTA, B_GREEN));
+  for (int cph = 0; cph < 16; ++cph) {
+    write_cell(nybble_to_hex(cph), POS((VGA_WIDTH - 16 - 1), (VGA_HEIGHT - (16 - cph))), COLOUR(RED, WHITE));
+    write_cell(nybble_to_hex(cph), POS((VGA_WIDTH - (16 - cph)), (VGA_HEIGHT - 16 - 1)), COLOUR(RED, WHITE));
+    for (int cpw = 0; cpw < 16; ++cpw) {
+      write_cell((cph * 16) + cpw, POS((VGA_WIDTH - (16 - cpw)), (VGA_HEIGHT - (16 - cph))), COLOUR(YELLOW, B_CYAN));
+    }
+  }
+}
+
+
 void main() {
 //  clear_scr();
 
